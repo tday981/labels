8b2ec49e5606f4af9a6997717cd3a93b15d7be88
Jason Pollard <jdp803@gmail.com>
2015-12-03 21:41:04 +0000

    added name.pl

---
+++ check_diff.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+use warnings;
+use List::MoreUtils qw(uniq);
+use Cwd;
+
+$wd=cwd();
+
+$q="$wd/quest/ddnLabels.xml";
+$p="$wd/new_pre_prod/ddnLabels.xml";
+
+@flist=("$q","$p");
+
+foreach $file (@flist) {
+
+	open $fh, '<', $file or die "Can't open file $file: $!\n";
+
+	@lines=<$fh>;
+
+	foreach $line (@lines) {
+
+		if ( $line =~ /\Qlabel ID\E/ ) {
+
+			@ar=split(/\"/,$line);
+			push @lab,$ar[1];
+			$la=$ar[1];
+
+		}
+
+		if ( $line =~ /\Qconsumer>\E/ && defined $la) {
+
+			@cons=split(/[<>]/,$line);
+			#print "$cons[2]\n";	
+		
+			if ( $file eq "$q" ) {
+	
+				$valq{$la}=$cons[2];
+
+			}
+		
+			if ( $file eq "$p" ) {
+	
+				$valp{$la}=$cons[2];
+
+			}
+
+
+		}
+
+		if ( $line =~ /\Q\/label>/ ) {
+
+			undef $la;
+
+		}
+
+	}
+}
+
+open $out, '>', "consumers.txt" or die "Can't open file: $!\n";
+foreach $l (sort { $a <=> $b } uniq(@lab)) {
+
+	if ( defined $valq{$l} ) {
+
+		print $out "quest:$l->$valq{$l}\n";
+	
+	}
+
+	if ( defined $valp{$l} ) {
+
+		print $out "pre_prod:$l->$valp{$l}\n\n";
+	
+	}
+
+}
+
